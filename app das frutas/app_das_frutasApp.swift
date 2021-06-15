5f7083d8bb1814fce47a085b53d4a7df5f464c50
schueda <a.schuedamenezes@gmail.com>
2021-06-15 08:52:21 -0300

    Initial Commit

---
+++ app das frutas/app_das_frutasApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_das_frutasApp.swift
+//  app das frutas
+//
+//  Created by André Schueda on 15/06/21.
+//
+
+import SwiftUI
+
+@main
+struct app_das_frutasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
