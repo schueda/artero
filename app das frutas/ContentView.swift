835eef8b5c55e28a215b2a9420b02306d6c06f39
schueda <a.schuedamenezes@gmail.com>
2021-06-17 09:44:08 -0300

    sets the camera and gallery access

--- app das frutas/ContentView.swift
+++ app das frutas/ContentView.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+        @State private var selectedImage: UIImage?
+        @State private var isImagePickerDisplay = false
+        
+        var body: some View {
+            NavigationView {
+                VStack {
+                    
+                    if selectedImage != nil {
+                        Image(uiImage: selectedImage!)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(Circle())
+                            .frame(width: 300, height: 300)
+                    } else {
+                        Image(systemName: "snow")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(Circle())
+                            .frame(width: 300, height: 300)
+                    }
+                    
+                    Button("Camera") {
+                        self.sourceType = .camera
+                        self.isImagePickerDisplay.toggle()
+                    }.padding()
+                    
+                    Button("photo") {
+                        self.sourceType = .photoLibrary
+                        self.isImagePickerDisplay.toggle()
+                    }.padding()
+                }
+                .navigationBarTitle("Demo")
+                .sheet(isPresented: self.$isImagePickerDisplay) {
+                    ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
+                }
+                
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ app das frutas/ImagePickerView.swift
@@ -0,0 +1,32 @@
+//
+//  ImagePicker.swift
+//  app das frutas
+//
+//  Created by André Schueda on 17/06/21.
+//
+
+import UIKit
+import SwiftUI
+
+struct ImagePickerView: UIViewControllerRepresentable {
+    
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var isPresented
+    var sourceType: UIImagePickerController.SourceType
+        
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = self.sourceType
+        imagePicker.delegate = context.coordinator as UIImagePickerControllerDelegate & UINavigationControllerDelegate // confirming the delegate
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+
+    }
+
+    // Connecting the Coordinator class with this struct
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(picker: self)
+    }
+}
