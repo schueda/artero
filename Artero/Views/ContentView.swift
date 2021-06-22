a3921b3d5547f6ed360375576ac840dd08a2f2c0
Mariana Florencio <mariana.florencio@outlook.com.br>
2021-06-22 11:42:43 -0300

    make card of the gallery

--- Artero/Views/ContentView.swift
+++ Artero/Views/ContentView.swift
@@ -20,28 +20,80 @@ struct ContentView: View {
                 NavigationLink(
                     destination: ThemeView(tema: "Amarelo"),
                     label : {
-                        Text("botao pro tema diario")
-                            .padding()
+                        
+                        ZStack {
+                            
+                            Image("art05")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 360, height: 280, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                .cornerRadius(12.0)
+                                .padding(4)
+                            
+                            VStack (alignment: .leading) {
+                            
+                                HStack  {
+                                    
+                                    VStack (alignment: .leading) {
+                                        
+                                        Text("TEMA DO DIA")
+                                            .font(.system(size: 20, weight: .bold, design: .default))
+                                            .foregroundColor(.white)
+                                        
+                                        Text("Amarelo")
+                                            .font(.system(size: 28, weight: .bold, design: .default))
+                                            .foregroundColor(.white)
+                                                                        
+                                    }
+                                    
+                                   // Spacer()
+                                 
+                                    Image(systemName:"chevron.right")
+                                        .font(.system(size: 18, weight: .bold, design: .default))
+                                        .foregroundColor(.white)
+                                    
+                                }
+                            
+                            Text("15 de junho")
+                                .font(.system(size: 13, weight: .bold, design: .default))
+                                .foregroundColor(.white)
+                            }
+                        }
+                        
                     })
                 
                 NavigationLink(
                     destination: ActivityView(),
                     label : {
-                        Text("botao pra tela de atividade")
-                            .padding()
+                        
+                        Rectangle()
+                            .frame(width: 360, height: 120, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .cornerRadius(12.0)
+                            .foregroundColor(.gray)
+                            .padding(4)
+                            
                     })
                 
                 NavigationLink(
                     destination: GalleryView(foto: "diatal"),
                     label : {
-                        Text("botao pra galeria")
-                            .padding()
+                         Rectangle()
+                        .frame(width: 360, height: 180, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(12.0)
+                        .foregroundColor(.gray)
+                            .padding(4)
+                          
                     })
+                
             }
-            .navigationBarTitle("Demo")
+           // .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+          //  .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+            .navigationBarTitle("Bom dia!")
+            
         }
         
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
