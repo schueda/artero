3c34e9d3c8ec7307781a8962f1e879445969c37f
Mariana Florencio <mariana.florencio@outlook.com.br>
2021-06-23 12:06:06 -0300

    adjust resolution for ALL iphones

--- Artero/Views/ContentView.swift
+++ Artero/Views/ContentView.swift
@@ -16,129 +16,15 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                NavigationLink(
-                    destination: ThemeView(tema: "Amarelo"),
-                    label : {
-                        
-                        ZStack {
-                            
-                            Image("art05")
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 370, height: 280, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .cornerRadius(12.0)
-                                .padding(4)
-                            
-                            VStack (alignment: .leading) {
-                            
-                                HStack  {
-                                    
-                                    VStack (alignment: .leading) {
-                                        
-                                        Text("TEMA DO DIA")
-                                            .font(.system(size: 20, weight: .bold, design: .default))
-                                            .foregroundColor(.white)
-                                        
-                                        Text("Amarelo")
-                                            .font(.system(size: 28, weight: .bold, design: .default))
-                                            .foregroundColor(.white)
-                                                                        
-                                    }
-                                    
-                                   // Spacer()
-                                 
-                                    Image(systemName:"chevron.right")
-                                        .font(.system(size: 18, weight: .bold, design: .default))
-                                        .foregroundColor(.white)
-                                    
-                                }
-                            
-                            Text("15 de junho")
-                                .font(.system(size: 13, weight: .bold, design: .default))
-                                .foregroundColor(.white)
-                            }
-                        }
-                        
-                    })
+            ZStack {
                 
-                NavigationLink(
-                    destination: ActivityView(),
-                    label : {
-                        
-                        ZStack {
-                            
-                            Rectangle()
-                                .frame(width: 370, height: 120, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                                .cornerRadius(12.0)
-                                .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
-                                .padding(4)
-                            
-                            VStack (alignment: .leading) {
-                            
-                                HStack  {
-                                    
-                                    Image(systemName:"sparkles")
-                                        .font(.system(size: 18, weight: .bold, design: .default))
-                                        .foregroundColor(.yellow)
-                                    
-                                    VStack (alignment: .leading) {
-                                        
-                                      
-                                        Text("ATIVIDADE")
-                                            .font(.system(size: 17, weight: .semibold, design: .default))
-                                            .foregroundColor(.gray)
-                                        
-                                        Text("Sem sequência")
-                                            .font(.system(size: 28, weight: .bold, design: .default))
-                                            .foregroundColor(.black)
-                                                                        
-                                    }
-                                    
-                                   // Spacer()
-                                 
-                                    Image(systemName:"chevron.right")
-                                        .font(.system(size: 18, weight: .bold, design: .default))
-                                        .foregroundColor(.white)
-                                    
-                                }
-                          
-                            }
-                        }
-                        
-                            
-                    })
-                
-                NavigationLink(
-                    destination: GalleryView(foto: "diatal"),
-                    label : {
-                        
-                        ZStack {
-                            
-                            Rectangle()
-                           .frame(width: 370, height: 180, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                           .cornerRadius(12.0)
-                           .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
-                               .padding(4)
-                            
-                            HStack {
-                            
-                            }
-                            
-                            Image(systemName:"chevron.right")
-                                .font(.system(size: 18, weight: .bold, design: .default))
-                                .foregroundColor(.white)
-                        }
-                          
-                    })
+                Color(UIColor.systemGray5).edgesIgnoringSafeArea(.bottom)
+            
+           HomeView()
                 
             }
-           // .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-          //  .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
-            .navigationBarTitle("Bom dia!")
-            
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
         }
-        
     }
     
 }
