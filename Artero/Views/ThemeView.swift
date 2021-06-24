479bbc9c4b2b1b479445ca3cad86fa22555c6adf
schueda <a.schuedamenezes@gmail.com>
2021-06-24 08:33:28 -0300

    implements the floating button

--- Artero/Views/ThemeView.swift
+++ Artero/Views/ThemeView.swift
@@ -8,32 +8,68 @@
 import SwiftUI
 
 struct ThemeView: View {
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var selectedImage: UIImage?
+    @State private var isImagePickerDisplay = false
     
     var photoRepository: PhotoRepository = PhotoDocumentRepository()
-    
-    @State private var selectedImage: UIImage?
     
     var tema: String
     
     var body: some View {
-        ScrollView {
-            NavigationLink(
-                destination: ImagePickerView(selectedImage: $selectedImage, sourceType: .photoLibrary),
-                label : {
-                    Text("camera!")
-                        .padding()
-                })
-            
-            if selectedImage != nil {
-                Image(uiImage: selectedImage!)
+        ZStack {
+            ScrollView {
+                Image("art05")
                     .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(Circle())
-                    .frame(width: 300, height: 300)
-                Button("save") {
-                    photoRepository.save(image: selectedImage!, withIdentifier: "background")
-                }
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 420)
+                
+                
             }
+            VStack {
+                Spacer()
+                Menu(content: {
+                    Button(action: {
+                        self.sourceType = .camera
+                        self.isImagePickerDisplay.toggle()
+                    }, label: {
+                        HStack {
+                            Image(systemName: "camera")
+                            Text("Tirar foto")
+                        }
+                    })
+                    Button(action: {
+                        self.sourceType = .photoLibrary
+                        self.isImagePickerDisplay.toggle()
+                    }, label: {
+                        HStack {
+                            Image(systemName: "photo.on.rectangle.angled")
+                            Text("Escolher foto ou vídeo")
+                        }
+                    })
+                }, label: {
+                    HStack {
+                        Image(systemName: "camera")
+                        Text("Adicionar mídia")
+                    }
+                    .frame(maxWidth: .infinity, minHeight: 50)
+                    .font(.system(size: 17, weight: .semibold, design: .default))
+                    .foregroundColor(.white)
+                    .background(Color(.link))
+                    .cornerRadius(10.0)
+                })
+                .padding(.horizontal)
+                .padding(.bottom, 50)
+            }
+        }.ignoresSafeArea()
+        .sheet(isPresented: self.$isImagePickerDisplay) {
+            ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
         }
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(tema: "telinha")
     }
 }
